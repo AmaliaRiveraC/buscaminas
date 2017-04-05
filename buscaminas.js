55d7c63485da36a76e9943cdf976bbe3d04aec42
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-04-04 23:15:15 -0500

    Empezando a trabajar con clases en lugar de id

--- buscaminas.js
+++ buscaminas.js
@@ -1,4 +1,4 @@
-var img0 = document.getElementById("img0");
+/*var img0 = document.getElementById("img0");
 var img1 = document.getElementById("img1");
 var img2 = document.getElementById("img2");
 var img3 = document.getElementById("img3");
@@ -15,43 +15,63 @@ var img13 = document.getElementById("img13");
 var img14 = document.getElementById("img14");
 var img15 = document.getElementById("img15");
 
-
 img0.addEventListener("click", bombas);
 img1.addEventListener("click", unaBombaCerca);
-img2.addEventListener("click", unaBombaCerca);
+img2.addEventListener("click", unaBombaCerca, true);
 img3.addEventListener("click", imagenClick);
 img4.addEventListener("click", unaBombaCerca);
 img5.addEventListener("click", dosBombasCerca);
-img6.addEventListener("click", bombas);
-img7.addEventListener("click", imagenClick);
+img6.addEventListener("click", bombas, true);
+img7.addEventListener("click", unaBombaCerca);
 img8.addEventListener("click", imagenClick);
-img9.addEventListener("click", bombas);
+img9.addEventListener("click", bombas, true);
 img10.addEventListener("click", dosBombasCerca);
 img11.addEventListener("click", imagenClick);
 img12.addEventListener("click", unaBombaCerca);
-img13.addEventListener("click", bombas);
+img13.addEventListener("click", bombas, true);
 img14.addEventListener("click", unaBombaCerca);
-img15.addEventListener("click", imagenClick);
+img15.addEventListener("click", imagenClick);*/
 
 
+var bombas = document.getElementsByClassName("bomba");
+var vacios = document.getElementsByClassName("vacio");
 
-
-
-function bombas(event){
-   this.src = "bomba.jpg";
-    alert("Has perdido");
-    event.stopPropagation();
+for(var j = 0; j < bombas.length; j++){
+	bombas[j].addEventListener("click", mostrarBomba);
 }
 
-function  unaBombaCerca(){
+for(var k = 0; k < vacios.length; k++){
+	vacios[k].addEventListener("click", mostrarVacio);
+}
+
+
+
+
+
+
+function mostrarBomba(event){
+	for(var j = 0; j < bombas.length; j++){
+		var imagen = document.createElement("img");
+        imagen.src = "bomba.jpg";
+        bombas[j].appendChild(imagen);	
+	}
+	alert("Has perdido");
+	event.stopPropagation(); 
+}
+
+function mostrarVacio (event) {
+	for(var k=0; k < vacios.length; k++){
+		var imagen = document.createElement("img");
+		imagen.src = "rosa.png";
+		vacios[k].appendChild(imagen); 
+	}
+}
+
+/*function  unaBombaCerca(){
     this.src = "uno.png";
 }
 
 function dosBombasCerca() {
     this.src= "dos.png";
 }
-function imagenClick (e) {
-    this.src = "rosa.png";
-
-    
-}
+*/
